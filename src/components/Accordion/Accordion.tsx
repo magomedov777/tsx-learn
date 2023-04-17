1765f8ffda41d70074c766a6f6c51cf3e1395f29
Arkadiy <alibullterrier@gmail.com>
2023-04-17 06:48:03 +0300

    add ul list

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -24,7 +24,11 @@ function AccordionTitle(props: AccordionTitlePropsType) {
 
 function AccordionBody() {
   return (
-  
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
   )
 };
 
